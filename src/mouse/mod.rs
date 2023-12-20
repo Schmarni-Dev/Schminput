6b97a9b1b11e534a66ff2ef4f2ccf8e0b7141a6b
Schmarni <marnistromer@gmail.com>
2023-12-20 21:39:40 +0100

    Whoops! forgot to git add the mouse folder

---
+++ src/mouse/mod.rs
@@ -0,0 +1,41 @@
+pub mod motion;
+pub mod mouse_binding_provider;
+
+use bevy::{
+    prelude::*,
+    utils::{HashMap, HashSet},
+};
+use mouse_binding_provider::MouseBinding;
+
+use crate::ActionTrait;
+
+use self::motion::MouseMotionAction;
+
+pub enum MouseBindingType {
+    Button(MouseBinding),
+    Motion,
+}
+#[derive(Resource, Default)]
+pub struct MouseBindings {
+    #[allow(clippy::type_complexity)]
+    bindings: HashMap<(&'static str, &'static str), Vec<MouseBinding>>,
+    motion_bindings: HashSet<(&'static str, &'static str)>,
+}
+
+impl MouseBindings {
+    // I don't understand why this 'static is needed or why it works
+    pub fn add_binding<T: 'static>(
+        &mut self,
+        action: &dyn ActionTrait<T = T>,
+        binding: MouseBinding,
+    ) {
+        self.bindings
+            .entry((action.action_key(), action.action_set_key()))
+            .or_default()
+            .push(binding);
+    }
+    pub fn add_motion_binding(&mut self, action: &dyn MouseMotionAction) {
+        self.motion_bindings
+            .insert((action.action_key(), action.action_set_key()));
+    }
+}
---
+++ src/mouse/motion.rs
@@ -0,0 +1,107 @@
+// Register extra fn on action trait with base action pass empty block for impl macro
+use bevy::{input::mouse::MouseMotion, prelude::*};
+
+use crate::ActionTrait;
+
+use super::MouseBindings;
+
+pub struct MouseMotionBindingProvider;
+impl Plugin for MouseMotionBindingProvider {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreUpdate, sync_mouse_moitions);
+    }
+}
+
+fn sync_mouse_moitions(
+    mut actions: Query<&mut dyn MouseMotionAction>,
+    bindings: Res<MouseBindings>,
+    mut mouse_events: EventReader<MouseMotion>,
+) {
+    let mut delta = Vec2::ZERO;
+    for ev in mouse_events.read() {
+        delta += ev.delta;
+    }
+    for mut action in &mut actions.iter_mut().flatten() {
+        if bindings
+            .motion_bindings
+            .contains(&(action.action_key(), action.action_set_key()))
+        {
+            let mut v = delta;
+            v.x *= action.mouse_sens_x();
+            v.y *= action.mouse_sens_y();
+            action.set_value(v);
+        }
+    }
+}
+
+#[bevy_trait_query::queryable]
+pub trait MouseMotionAction: ActionTrait<T = Vec2> {
+    fn mouse_sens_x(&self) -> f32;
+    fn mouse_sens_y(&self) -> f32;
+}
+
+#[macro_export]
+/// new_action!(
+/// |   action_ident: ident,
+/// |   action_key: &'static str,
+/// |   action_name: expression -> String,
+/// |   action_set_key: expression -> &'static str,
+/// |   action_set_name: expression -> String
+/// )
+macro_rules! mouse_action {
+    ($ident:ident, $key:literal, $name:expr, $set_key:expr, $set_name:expr) => {
+        #[derive(bevy::prelude::Component)]
+        pub struct $ident {
+            data: bevy::prelude::Vec2,
+            previous_data: bevy::prelude::Vec2,
+            mouse_sens_x: f32,
+            mouse_sens_y: f32,
+        }
+
+        impl Default for $ident {
+            fn default() -> Self {
+                Self {
+                    mouse_sens_x: 1.0,
+                    mouse_sens_y: 1.0,
+                    data: bevy::prelude::Vec2::ZERO,
+                    previous_data: bevy::prelude::Vec2::ZERO,
+                }
+            }
+        }
+
+        bevy_schminput::gen_action_trait_impl!(
+            $ident,
+            bevy::prelude::Vec2,
+            $key,
+            $name,
+            $set_key,
+            $set_name
+        );
+        bevy_schminput::gen_mouse_action_trait_impl!($ident);
+        impl bevy_schminput::ActionExtensionTrait for $ident {
+            fn register_other_trait(app: &mut App)
+            where
+                Self: Sized,
+            {
+                use bevy_schminput::prelude::RegisterExt;
+                app.register_component_as::<dyn bevy_schminput::mouse::motion::MouseMotionAction, Self>();
+            }
+        }
+    };
+}
+
+#[macro_export]
+/// new_action!(action_ident: ident)
+macro_rules! gen_mouse_action_trait_impl {
+    ($ident:ident) => {
+        impl bevy_schminput::mouse::motion::MouseMotionAction for $ident {
+            fn mouse_sens_x(&self) -> f32 {
+                self.mouse_sens_x
+            }
+
+            fn mouse_sens_y(&self) -> f32 {
+                self.mouse_sens_y
+            }
+        }
+    };
+}
