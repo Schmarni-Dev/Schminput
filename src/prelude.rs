6bc996b8d9012e6443eba76ad9805884a5b7476d
Schmarni <marnistromer@gmail.com>
2024-03-30 01:26:22 +0100

    basic MnK support

---
+++ src/prelude.rs
@@ -0,0 +1,4 @@
+pub use crate::keyboard::{KeyboardBinding, KeyboardBindings};
+pub use crate::mouse::{MouseBindings, MouseButtonBinding, MouseMotionBinding, MouseMotionType};
+pub use crate::{ActionId, ActionName, ActionSet};
+pub use crate::{BoolActionValue, F32ActionValue, Vec2ActionValue};
