e06800c1b84b2ca6ef71c76a42fa2e43d66d6576
Schmarni <marnistromer@gmail.com>
2024-09-03 14:00:01 +0200

    restructure a little
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- schminput_rebinding/src/lib.rs
+++ schminput_rebinding/src/lib.rs
@@ -1,14 +1,17 @@
+pub mod config;
 pub mod default_bindings;
+#[cfg(feature = "egui")]
 pub mod egui;
+#[cfg(feature = "egui")]
 pub mod egui_window;
 pub mod persistent_bindings;
 pub mod runtime_rebinding;
 pub mod str_converstions;
-pub mod config;
 
 use bevy::{app::PluginGroupBuilder, prelude::*};
 use config::SchminputConfigPlugin;
 use default_bindings::RebindingDefaultBindingsPlugin;
+#[cfg(feature = "egui")]
 use egui_window::RebindingEguiWindowPlugin;
 use persistent_bindings::PersistentBindingsPlugin;
 use runtime_rebinding::RuntimeRebindingPlugin;
@@ -21,8 +24,14 @@ impl PluginGroup for DefaultSchminputRebindingPlugins {
             .add(RuntimeRebindingPlugin)
             .add(PersistentBindingsPlugin)
             .add(SchminputConfigPlugin)
-            .add(RebindingEguiWindowPlugin)
             .add(RebindingDefaultBindingsPlugin);
+
+        #[cfg(feature = "egui")]
+        #[allow(clippy::unnecessary_operation)]
+        {
+            plugins = plugins.add(RebindingEguiWindowPlugin)
+        };
+
         plugins
     }
 }
